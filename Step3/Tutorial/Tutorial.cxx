3c559ac1512c36d94b11391cc805fb4674f79494
accuracy-maker <z5506409@ad.unsw.edu.au>
2026-05-05 12:35:55 +1000

    FINSHED Step3

--- Step3/Tutorial/Tutorial.cxx
+++ Step3/Tutorial/Tutorial.cxx
@@ -2,6 +2,8 @@
 
 // TODO3: Include <format>
 
+#include <format>
+
 #include <iostream>
 #include <string>
 
@@ -11,7 +13,8 @@ int main(int argc, char* argv[])
 {
   if (argc < 2) {
     // TODO4: Convert the print to use std::format
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    // std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    std::cout << std::format("Usage {} number\n", argv[0]);
     return 1;
   }
 
@@ -21,6 +24,7 @@ int main(int argc, char* argv[])
   // calculate square root
   double const outputValue = mathfunctions::sqrt(inputValue);
   // TODO5: Convert the print to use std::format
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
+  // std::cout << "The square root of " << inputValue << " is " << outputValue
+  //           << std::endl;
+  std::cout << std::format("The square root of {} is {}\n", inputValue, outputValue);
 }
